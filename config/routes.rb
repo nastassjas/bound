4c2c95d9835deac9fc47cce1b8cd327ebfa31ecb
Vanessa Eldoh <van.eldoh@gmail.com>
2019-06-04 11:47:49 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'missions#home'
   devise_for :users
-  root to: 'pages#home'
+  resources :associations, only: :show do
+    resources :missions, only: :create
+  end
+  resources :missions, only:[:index, :show] do
+    resources :bookings, only: :create
+  end
+  resources :user_skills, only: :create
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
