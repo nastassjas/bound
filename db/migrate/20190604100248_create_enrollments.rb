f885e8fc56b8479b3381f90b3dc627e3ae2947c2
Vanessa Eldoh <van.eldoh@gmail.com>
2019-06-04 12:17:21 +0200

    routes models controllers

---
+++ db/migrate/20190604100248_create_enrollments.rb
@@ -0,0 +1,11 @@
+class CreateEnrollments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :enrollments do |t|
+      t.references :association, foreign_key: true
+      t.references :user, foreign_key: true
+      t.string :admin
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20190604100631_create_missions.rb
@@ -0,0 +1,17 @@
+class CreateMissions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :missions do |t|
+      t.string :title
+      t.text :description
+      t.datetime :start_time
+      t.datetime :end_time
+      t.integer :volunteers_count
+      t.string :address
+      t.references :association, foreign_key: true
+      t.string :category
+      t.references :skill, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
