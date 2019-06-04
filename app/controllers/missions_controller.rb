d85bce87aebf0a139f8df45e92572fd57ade9f44
Nastassja Swierczynski <nastassja.swierczynski@gmail.com>
2019-06-04 17:01:47 +0200

    cards to be finished

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,2 +1,19 @@
 class MissionsController < ApplicationController
+  def index
+    @missions = Mission.all
+  end
+
+  def show
+    @mission = Mission.find(params[:id])
+  end
+
+  def home
+    @missions = Mission.all
+  end
+
+  private
+
+  def mission_params
+    params.require(:mission).permit(:title, :description, :address, :latitude, :longitude)
+  end
 end
