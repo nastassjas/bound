b112aa295af921e7d7d24b26d0136e2734f0b190
MARIEL BA <marielba82@yahoo.fr>
2019-06-05 16:31:59 +0200

    projects controller done

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,10 @@
 class ProjectsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:home, :index, :show]
+  skip_after_action :verify_authorized, only: [:home, :index, :show]
+
+  def home
+    @projects = policy_scope(Project)
+  end
 
   def index
     @projects = policy_scope(Project)
@@ -9,8 +14,23 @@ class ProjectsController < ApplicationController
     @project = Project.new
   end
 
+  def create
+    @project = Project.new(project_params)
+    @project.user = current_user
+    if @project.save!
+      redirect_to projects_path
+    else
+      render :new
+    end
+  end
+
   def show
     @project = Project.find(params[:id])
   end
-end
 
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :address, :charity, :category)
+  end
+end
