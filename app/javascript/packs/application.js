c15477be9648ca6522e08e0fe05441fd4f75fb4e
MARIEL BA <marielba82@yahoo.fr>
2019-06-06 12:39:57 +0200

    map and markers uploaded

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,9 @@ import "bootstrap";
 
 import { initAutocomplete } from '../plugins/init_autocomplete';
 initAutocomplete();
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_111004) do
+ActiveRecord::Schema.define(version: 2019_06_06_084817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,8 @@ ActiveRecord::Schema.define(version: 2019_06_05_111004) do
     t.text "description"
     t.string "address"
     t.string "category"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["charity_id"], name: "index_projects_on_charity_id"
   end
 
