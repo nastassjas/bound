c932512aa1d84d456e96a56a76f8f6468b439b38
Patricia Falandysz <pfalandysz@yahoo.com>
2019-06-09 03:16:41 +0200

    new change home

--- app/javascript/plugins/mouse.js
+++ app/javascript/plugins/mouse.js
@@ -18,3 +18,16 @@ $(document).on("mousemove", function (event) {
   })
 
 })
+
+var count = 0
+var images = ["https://res.cloudinary.com/patti01/image/upload/v1560030656/govo/arrow-final_q62hf1.svg","https://res.cloudinary.com/patti01/image/upload/v1560041305/govo/wri-ar_znuolg.svg"]
+
+setInterval(function () {
+  count = count + 1
+  count = count % images.length
+
+  var image = images[count]
+
+  $(".moove").attr("src", image)
+
+}, 3000)
