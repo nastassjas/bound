71856f589488e28b6afef1f7bfd1768ec7291c84
MARIEL BA <marielba82@yahoo.fr>
2019-06-11 12:45:30 +0200

    searchbar news

--- app/javascript/components/toggle_map.js
+++ app/javascript/components/toggle_map.js
@@ -1,32 +1,37 @@
-const mapOn = document.querySelector(".map-on");
-const mapOff = document.querySelector(".map-off");
+const toggleMap = () => {
+  const mapOn = document.querySelector(".map-on");
+  const mapOff = document.querySelector(".map-off");
 
-const cardsGrid = document.querySelector(".container-cards-2");
-const mapContainer = document.getElementById("map");
+  const cardsGrid = document.querySelector(".container-cards-2");
+  const mapContainer = document.getElementById("map");
 
-console.log(mapContainer);
+  // console.log(mapContainer);
 
 
-const hideMap = () => {
-  mapContainer.classList.add('hidemap')
-  mapContainer.classList.remove('showmap')
+  const hideMap = () => {
+    mapContainer.classList.add('hidemap');
+    mapContainer.classList.remove('showmap');
 
-  cardsGrid.classList.add('large')
+    cardsGrid.classList.add('large');
+  }
+
+  const showMap = () => {
+    mapContainer.classList.remove('hidemap');
+    mapContainer.classList.add('showmap');
+
+    cardsGrid.classList.remove('large');
+  }
+
+
+
+  mapOn.addEventListener('click', (e) => {
+    showMap();
+  })
+
+  mapOff.addEventListener('click', (e) => {
+    hideMap();
+  })
 }
 
-const showMap = () => {
-  mapContainer.classList.remove('hidemap')
-  mapContainer.classList.add('showmap')
 
-  cardsGrid.classList.remove('large')
-}
-
-
-
-mapOn.addEventListener('click', (e) => {
-  showMap();
-})
-
-mapOff.addEventListener('click', (e) => {
-  hideMap();
-})
+export { toggleMap };
