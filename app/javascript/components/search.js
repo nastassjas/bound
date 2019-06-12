7e681c8f68d9a1964d01514ff7d28aa6b0347041
MARIEL BA <marielba82@yahoo.fr>
2019-06-12 16:35:27 +0200

    submit address autocomplete

--- app/javascript/components/search.js
+++ app/javascript/components/search.js
@@ -1,18 +1,18 @@
 import Rails from 'rails-ujs';
 // Rails.start()
 const addressSubmit = () => {
-  const addressForm = document.querySelector('.address-form');
-  const addressFormInputValue = document.querySelector('#address').value;
-  const dateInputValue = document.querySelector('#date-picker').value;
-  const categoryInputValue = document.querySelector('#category-search').value;
-  const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+  // const addressForm = document.querySelector('.address-form');
+  // const addressFormInputValue = document.querySelector('#address').value;
+  // const dateInputValue = document.querySelector('#date-picker').value;
+  // const categoryInputValue = document.querySelector('#category-search').value;
+  // const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
-  if (addressForm) {
-    const algoliaDiv = document.querySelector('.ap-dataset-places');
-    algoliaDiv.addEventListener('click', (event) => {
-      addressForm.dispatchEvent(new Event('submit', {bubbles: true, cancelable: true}));
-    });
-  }
+  // if (addressForm) {
+  //   const algoliaDiv = document.querySelector('.ap-dataset-places');
+  //   algoliaDiv.addEventListener('click', (event) => {
+  //     addressForm.dispatchEvent(new Event('submit', {bubbles: true, cancelable: true}));
+  //   });
+  // }
 };
 
 const categorySubmit = () => {
--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -4,8 +4,8 @@ const initAutocomplete = () => {
   const addressInput = document.getElementById('address');
   if (addressInput) {
     const placesAutocomplete = places({ container: addressInput, userDeviceLocation: true });
+    placesAutocomplete.on('change', e => document.getElementById('refresh-index').click());
   }
-
 };
 
 export { initAutocomplete };
