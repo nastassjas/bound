f5bf4b83d8d0b81039dfbb1176b1655aa5e5e068
MARIEL BA <marielba82@yahoo.fr>
2019-06-12 16:13:11 +0200

    flatpickr auto submit

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -4,7 +4,6 @@ const initAutocomplete = () => {
   const addressInput = document.getElementById('address');
   if (addressInput) {
     const placesAutocomplete = places({ container: addressInput, userDeviceLocation: true });
-    placesAutocomplete.on('change', e => document.querySelector('.address-form').submit());
   }
 
 };
